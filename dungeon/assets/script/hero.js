f3ef52f66a34f0c20265fd7f04db5a24e877e5af
zjp693 <942680978@qq.com>
2022-02-22 19:38:24 +0800

    地图制作&人物移动

---
+++ dungeon/assets/script/hero.js
@@ -0,0 +1,119 @@
+//教程
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+//存放按钮按下的状态
+const Input = {};
+cc.Class({
+  extends: cc.Component,
+
+  properties: {
+    // foo: {
+    //     // ATTRIBUTES:
+    //     default: null,        // The default value will be used only when the component attaching
+    //                           // to a node for the first time
+    //     type: cc.SpriteFrame, // optional, default is typeof default
+    //     serializable: true,   // optional, default is true
+    // },
+    // bar: {
+    //     get () {
+    //         return this._bar;
+    //     },
+    //     set (value) {
+    //         this._bar = value;
+    //     }
+    // },
+  },
+
+  // LIFE-CYCLE CALLBACKS:
+
+  onLoad() {
+    //   初始化人物移动速度
+    this._speed = 200;
+    // 人物移动状态
+    this.sp = cc.v2(0, 0);
+    // 设置默认的状态属性
+    this.state = "";
+    // 获取动画组件
+    this.heroAni = this.node.getComponent(cc.Animation);
+    // this.heroAni = this.node.getComponent(cc.Animation);
+    console.log(this.heroAni);
+    //   监听按键
+    cc.systemEvent.on("keydown", this.onkeydown, this);
+    cc.systemEvent.on("keyup", this.onkeyup, this);
+  },
+  //改变当前角色的状态
+  setState(state) {
+    //   判断传入以当前的是否相等
+    if (this.state == state) return;
+    this.state = state;
+    // 播放动画
+    this.heroAni.play(this.state);
+  },
+  //   按下
+  onkeydown(e) {
+    Input[e.keyCode] = 1;
+  },
+  //   松开
+  onkeyup(e) {
+    Input[e.keyCode] = 0;
+  },
+  update(dt) {
+    //#region 人物移动逻辑
+    if (Input[cc.macro.KEY.a] || Input[cc.macro.KEY.left]) {
+      console.log("向左");
+      this.sp.x = -1;
+      //   this.node.x -= this._speed * dt;
+    } else if (Input[cc.macro.KEY.d] || Input[cc.macro.KEY.right]) {
+      console.log("向右");
+      this.sp.x = 1;
+      //   this.node.x += this._speed * dt;
+    } else {
+      this.sp.x = 0;
+      //   console.log(this.sp.x);
+    }
+
+    if (Input[cc.macro.KEY.w] || Input[cc.macro.KEY.up]) {
+      console.log("向上");
+      this.sp.y = 1;
+    } else if (Input[cc.macro.KEY.s] || Input[cc.macro.KEY.down]) {
+      console.log("向下");
+      this.sp.y = -1;
+      //   this.node.y -= this._speed * dt;
+    } else {
+      this.sp.y = 0;
+    }
+    if (this.sp.x) {
+      this.node.x += this.sp.x * this._speed * dt;
+    } else if (this.sp.y) {
+      this.node.y += this.sp.y * this._speed * dt;
+    } else {
+    }
+    // 具体的动画帧
+    let state = "";
+    if (this.sp.x == 1) {
+      state = "hero_right";
+    } else if (this.sp.x == -1) {
+      state = "hero_left";
+    } else if (this.sp.y == 1) {
+      state = "hero_up";
+    } else if (this.sp.y == -1) {
+      state = "hero_down";
+    }
+    // 调用动画
+    if (state) {
+      this.setState(state);
+    }
+
+    //#endregion
+  },
+  // start () {
+
+  // },
+
+  // update (dt) {},
+});
